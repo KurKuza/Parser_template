c87e2075c1f794a0ce976af26fdaee3d7c995b38
KurKuza <47026225+KurKuza@users.noreply.github.com>
2022-01-22 20:08:18 +0300

    Add files via upload

--- parser_template.py
+++ parser_template.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
-headers = ["User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"]
+headers = {"accept-ranges": "bytes",
+           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"}
+url = "http://www.correctenglish.ru/theory/writing/contractions/"
 
-# Получение контента
-q = requests.get(url)
-result = q.content
+req = requests.get(url, headers= headers)
+src = req.text 
 
-# lxml тип парсинга
-soup = BeautifulSoup(result, 'lxml')
+with open("index.html", "w") as file:
+    file.write(src)
+
+soup = BeautifulSoup(req, 'lxml')
+print(src)
